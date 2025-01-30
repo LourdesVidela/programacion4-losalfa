3d4921085a0f43aef974893643db6d149a19f6b6
Rosana Mauno <rosanammauno@gmail.com>
2025-01-30 02:46:00 -0300

    crearcion entorno virtual

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\programacion4-losalfa\venv
+set VIRTUAL_ENV=C:\Users\Anyula\Desktop\programacion4-losalfa\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
